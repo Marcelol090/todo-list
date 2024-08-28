9317e54ec5f3e8c97b325eb5027e6ac8f71674ab
Marcelol090 <rengarmarcelo.mgh@gmail.com>
2024-08-27 22:16:15 -0300

    Criado o crypt da senha

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,39 +1,27 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import {
+  getUserService,
+  createUserService,
+} from "@/src/modules/users/services/user.service";
 
 export async function GET() {
-  const users = await prisma.user.findMany();
+  const users = await getUserService();
   return NextResponse.json({ users });
 }
 
 export async function POST(req: Request) {
   try {
-    const { email, senha } = await req.json();
-    console.log(email, senha);
-
-    // Verifica se o usuário já existe
-    const existingUser = await prisma.user.findUnique({
-      where: { email },
-    });
-
-    if (existingUser) {
-      return NextResponse.json(
-        { error: "Usuário já existe." },
-        { status: 400 }
+    const { email, senha, confirmarSenha } = await req.json();
+    
+    const user = await createUserService(email, senha, confirmarSenha);
+  
+    if(user === undefined){
+      NextResponse.json(
+        { error: "Erro ao criar usuário." },
+        { status: 500 }
       );
     }
-
-    // Cria um novo usuário
-    const newUser = await prisma.user.create({
-      data: {
-        email,
-        senha,
-      },
-    });
-
-    return NextResponse.json({ user: newUser });
+    return NextResponse.json({ user });
   } catch (error) {
     console.error("Erro ao criar usuário:", error);
     return NextResponse.json(
