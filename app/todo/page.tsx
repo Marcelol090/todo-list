73e6cb8cd4f14420751ad6f309f853505a5becfd
Marcelol090 <rengarmarcelo.mgh@gmail.com>
2024-09-12 22:26:24 -0300

    Criado a logica do editar o todolist

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { Button } from "@/src/components/Button/Button";
 import { ItemList } from "@/src/components/ItemList/ItemsList";
@@ -7,23 +7,35 @@ import ButtonMore from "@/src/components/Icons/ButtonMore";
 import Trash from "@/src/components/Icons/Trash";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { CircleArrowLeft, Pencil, SquarePlus, Trash2 } from "lucide-react";
 
 export default function Todo() {
-
   return (
     <main className="flex-1 h-full flex flex-col">
       <header className="items-center flex justify-between border-b-2 border-[#54353ECC] pb-4 mt-16">
         <h1 className="text-lg items-center max-w-xs gap-6 flex  font-bold text-[#FEEDE1] font-poppins">
-          <Link href={"/"} className="w-max"><ArrowLeftIcon /></Link>
-          <span className="truncate max-w-full">😎 Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptatem.</span>
+          <Link href={"/"} className="w-8 h-8 text-[#462730] hover:text-[#F25551]">
+          <CircleArrowLeft />
+          </Link>
+
+          <span className="truncate max-w-full">
+            😎 Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
+            voluptatem.
+          </span>
         </h1>
+
         <div className="flex gap-6 ">
-          <Button className="flex gap-4 text-[#F25551] h-6 items-center">
-            <Trash />
+          <Button className="flex gap-4 text-[#F25551] hover:text-[#9d3836]">
+            <Pencil />
+            Edit
+          </Button>
+
+          <Button className="flex gap-4 text-[#F25551] hover:text-[#9d3836] h-6 items-center">
+            <Trash2 />
             Delete List
           </Button>
-          <Button className="flex gap-4 text-[#F25551] h-6 items-center">
-            <ButtonMore />
+          <Button className="flex gap-4 text-[#F25551] hover:text-[#9d3836] h-6 items-center">
+            <SquarePlus />
             Add to-do
           </Button>
         </div>
--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,27 +1,18 @@
 import EmojiPicker from "emoji-picker-react";
+import { ListType } from "@/src/modules/todo-list/types/ListType";
 
-
-import { Pencil } from "lucide-react";
+import { Check, ChevronRight, Pencil } from "lucide-react";
 import { useState } from "react";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/src/components/ui/dialog";
-import {
   Select,
+  SelectLabel,
+  SelectTrigger,
   SelectContent,
   SelectItem,
-  SelectTrigger,
-  SelectValue,
+  SelectGroup,
 } from "@/src/components/ui/select";
-import { InputLabel } from "@/src/components/InputLabel/InputLabel";
-import { useRouter } from "next/navigation";
-import { ListType } from "@/src/modules/todo-list/types/ListType";
 import { useUpdateList } from "@/src/modules/todo-list/use-querys/useUpdateList";
+import { useRouter } from "next/navigation";
 
 type CardListProps = {
   finished: boolean;
@@ -37,31 +28,179 @@ export default function CardList({
   userId,
 }: CardListProps) {
   const router = useRouter();
+  const [isEditing, setIsEditing] = useState(false);
 
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [emoji, setEmoji] = useState(listEmoji);
+  const [listNameInput, setListNameInput] = useState(listName);
+  const [selectedPriority, setSelectedPriority] = useState(priority);
   const [finished, setFinished] = useState(initialFinished);
 
   const mutation = useUpdateList({
-    onMutate: async () => {
-      setFinished(!finished);
-    },
-    onError: (error: any) => {
+    onError: (error) => {
       console.error(error);
     },
   });
 
+  const handleEmojiClick = () => {
+    setShowEmojiPicker(!showEmojiPicker);
+  };
+
+  const handleEmojiChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmoji(event.target.value);
+  };
+
+  const handleListNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setListNameInput(event.target.value);
+  };
+
+  const handleSave = () => {
+    mutation.mutate({
+      listId: listId,
+      userId: userId,
+      finished: finished,
+      listEmoji: emoji,
+      listName: listNameInput,
+      priority: selectedPriority,
+    });
+    setIsEditing(!isEditing);
+  };
+
+  if (!isEditing) {
+    return (
+      <div className="w-full p-4">
+        <div
+          className={`mb-2 flex cursor-pointer items-center justify-between rounded-md border-l-4 ${
+            selectedPriority === "Alta"
+              ? "border-red-600"
+              : selectedPriority === "Media"
+              ? "border-yellow-500"
+              : "border-green-500"
+          } bg-[#2D1B30] p-4 text-white shadow-lg transition-all duration-200`}
+        >
+          <div className="flex items-center">
+            <span role="img" aria-label="list" className="mr-2">
+              {emoji}
+            </span>
+            <span>{listNameInput}</span>
+          </div>
+          <div className="flex items-center justify-center gap-2">
+            <div className="relative mr-2 inline-block w-10 select-none align-middle">
+              <input
+                type="checkbox"
+                className="hidden"
+                id={`list-finished${listId}`}
+                readOnly
+                checked={finished}
+                onClick={() => {
+                  mutation.mutate({
+                    listId: listId,
+                    userId: userId,
+                    finished: finished ? false : true,
+                  });
+                  setFinished(!finished);
+                }}
+              />
+              <label
+                htmlFor={`list-finished${listId}`}
+                className={`flex h-6 cursor-pointer items-center overflow-hidden rounded-full ${
+                  finished ? "justify-end" : "justify-start"
+                } ${
+                  finished ? "bg-[#50F283]" : "bg-[#F25551]"
+                } border shadow-xl transition ${
+                  finished ? "border-[#50F283]" : "border-[#F25551]"
+                } `}
+              >
+                <span
+                  className={`flex h-6 font-semibold ${
+                    finished ? "text-black" : "text-white"
+                  } w-6 items-center justify-center overflow-hidden rounded-full text-center text-xs ${
+                    finished ? "bg-white" : "bg-[#462730]"
+                  } transition`}
+                >
+                  {finished ? "F" : "NF"}
+                </span>
+              </label>
+            </div>
+
+            <span
+              className="text-[#FFFBFF] hover:text-[#FEEDE1]"
+              onClick={() => setIsEditing(!isEditing)}
+            >
+              <Pencil />
+            </span>
+            <span
+              className="text-[#FFFBFF] hover:text-[#FEEDE1]"
+              onClick={() => router.push(`/todo`)}
+            >
+              <ChevronRight />
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="w-full p-4">
       <div
-        className={`mb-2 flex cursor-pointer items-center justify-between rounded-md border-l-4 ${priority === "Alta" ? "border-red-600" : priority === "Media" ? "border-yellow-500" : "border-green-500"} bg-[#2D1B30] p-4 text-white shadow-lg transition-all duration-200`}
+        className={`mb-2 flex cursor-pointer items-center justify-between rounded-md border-l-4 ${
+          selectedPriority === "Alta"
+            ? "border-red-600"
+            : selectedPriority === "Media"
+            ? "border-yellow-500"
+            : "border-green-500"
+        } bg-[#2D1B30] p-4 text-white shadow-lg transition-all duration-200`}
       >
         <div className="flex items-center">
-          <span role="img" aria-label="list" className="mr-2">
-            {listEmoji}
+          <span
+            role="img"
+            aria-label="list"
+            className="mr-2 cursor-pointer"
+            onClick={handleEmojiClick}
+          >
+            {emoji}
           </span>
-          <span>{listName}</span>
+          {showEmojiPicker && (
+            <div className="relative">
+              <div className="absolute left-0 top-0">
+                <EmojiPicker
+                  onEmojiClick={(emojiObject) => {
+                    setEmoji(emojiObject.emoji);
+                    setShowEmojiPicker(false);
+                  }}
+                />
+              </div>
+            </div>
+          )}
+          <input
+            className="rounded border bg-transparent px-2"
+            type="text"
+            value={listNameInput}
+            onChange={handleListNameChange}
+          />
         </div>
-        <div className="flex items-center justify-center gap-2">
-          <div className="relative mr-2 inline-block w-10 select-none align-middle">
+
+        <div className="flex w-max items-center justify-center gap-2">
+          <Select
+            value={selectedPriority}
+            onValueChange={(value) => {
+              setSelectedPriority(value as "Alta" | "Media" | "Baixa");
+            }}
+          >
+            <SelectTrigger className="min-w-20 rounded border bg-transparent px-2">
+              <span className="text-sm font-semibold">{selectedPriority}</span>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Priority</SelectLabel>
+                <SelectItem value="Alta">Alta</SelectItem>
+                <SelectItem value="Media">Media</SelectItem>
+                <SelectItem value="Baixa">Baixa</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <div className="relative mr-2 inline-block w-10 min-w-max select-none align-middle">
             <input
               type="checkbox"
               className="hidden"
@@ -69,19 +208,25 @@ export default function CardList({
               readOnly
               checked={finished}
               onClick={() => {
-                mutation.mutate({
-                  listId: listId,
-                  userId: userId,
-                  finished: finished ? false : true,
-                });
+                setFinished(!finished);
               }}
             />
             <label
               htmlFor={`list-finished${listId}`}
-              className={`flex h-6 cursor-pointer items-center overflow-hidden rounded-full ${finished ? "justify-end" : "justify-start"} ${finished ? "bg-[#50F283]" : "bg-[#F25551]"} border shadow-xl transition ${finished ? "border-[#50F283]" : "border-[#F25551]"} `}
+              className={`flex h-6 min-w-10 cursor-pointer items-center overflow-hidden rounded-full ${
+                finished ? "justify-end" : "justify-start"
+              } ${
+                finished ? "bg-[#50F283]" : "bg-[#F25551]"
+              } border shadow-xl transition ${
+                finished ? "border-[#50F283]" : "border-[#F25551]"
+              } `}
             >
               <span
-                className={`flex h-6 font-semibold ${finished ? "text-black" : "text-white"} w-6 items-center justify-center overflow-hidden rounded-full text-center text-xs ${finished ? "bg-white" : "bg-[#462730]"} transition`}
+                className={`flex h-6 font-semibold ${
+                  finished ? "text-black" : "text-white"
+                } w-6 items-center justify-center overflow-hidden rounded-full text-center text-xs ${
+                  finished ? "bg-white" : "bg-[#462730]"
+                } transition`}
               >
                 {finished ? "F" : "NF"}
               </span>
@@ -90,9 +235,9 @@ export default function CardList({
 
           <span
             className="text-[#FFFBFF] hover:text-[#FEEDE1]"
-            onClick={() => router.push(`/list/${listId}`)}
+            onClick={handleSave}
           >
-            <Pencil />
+            <Check />
           </span>
         </div>
       </div>
